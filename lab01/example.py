87d5c54297f674faf60c625e014f1ecbfecbc87f
Dmitry <cher-di@mail.ru>
2020-04-18 21:03:03 +0500

    [lab01] Removed ContinuosMove from lab01

--- lab01/example.py
+++ lab01/example.py
@@ -1,4 +1,5 @@
 import zeep_fix
+
 from onvif import ONVIFCamera, ONVIFService
 from zeep import helpers
 
@@ -28,28 +29,3 @@ if __name__ == '__main__':
     moverequest.Position.PanTilt.y = 0.7
     moverequest.Position.Zoom = 0
     ptz_service.AbsoluteMove(moverequest)
-
-    # # Делаем ContinuousMove
-    # moverequest = ptz_service.create_type('ContinuousMove')
-    # moverequest.Pro   fileToken = tokens["Profile"]
-    # moverequest.Velocity = ptz_service.GetStatus({'ProfileToken': tokens["Profile"]}).Position
-    # moverequest.Velocity.PanTilt.x = 0
-    # moverequest.Velocity.PanTilt.y = -1
-    # moverequest.Velocity.Zoom.x = 0
-    # moverequest.Velocity.PanTilt.space = ''
-    # moverequest.Velocity.Zoom.space = ''
-    # ptz_service.ContinuousMove(moverequest)
-    #
-    # # Меняем фокус
-    # moverequest = imaging_service.create_type('Move')
-    # moverequest.VideoSourceToken = tokens["VideoSourceConfigurationSourceToken"]
-    # moverequest.Focus = imaging_service.GetMoveOptions(
-    #     {'VideoSourceToken': tokens["VideoSourceConfigurationSourceToken"]}).Continuous
-    # print(moverequest.Focus)
-    # imaging_service.Move(moverequest)
-
-    # for profile in profiles_dict:
-    #     delete_rec(profile)
-    #
-    # with open("onvif.json", "wt") as file:
-    #     json.dump(profiles_dict[0], file, indent=4)
