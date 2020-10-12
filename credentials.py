ab2d16bb06aed60693fb3720e2fb7242687104ea
Dmitry <cher-di@mail.ru>
2020-10-13 01:55:00 +0500

    Update credentials

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 IP = "172.18.212.17"  # Camera IP address
 PORT = 80  # Port
-USER = "laba2102"  # Username
-PASS = "TMPpassword"  # Password
+USER = "admin"  # Username
+PASS = "Supervisor"  # Password
